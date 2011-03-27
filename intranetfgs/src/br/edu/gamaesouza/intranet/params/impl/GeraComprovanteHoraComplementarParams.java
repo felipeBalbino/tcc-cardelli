b59de6f3d275fddd82822b46ba816ddd57b7a840
svn: gabrielcardelli git: gabrielcardelli <gabrielcastilho.cardelli@gmail.com>
2011-03-27 00:19:11 +0000

    Adicionado novo Param

---
+++ intranetfgs/src/br/edu/gamaesouza/intranet/params/impl/GeraComprovanteHoraComplementarParams.java
@@ -0,0 +1,37 @@
+package br.edu.gamaesouza.intranet.params.impl;
+
+import br.edu.gamaesouza.intranet.bean.Aluno;
+import br.edu.gamaesouza.intranet.bean.HoraComplementar;
+
+public class GeraComprovanteHoraComplementarParams {
+	
+	private Integer alunoId;
+	private Integer horaId;
+	
+	public Aluno getAluno(){
+		return null;
+	}
+	
+	public HoraComplementar getHoraComplementar(){
+		return null;
+	}
+
+	public Integer getAlunoId() {
+		return alunoId;
+	}
+
+	public void setAlunoId(Integer alunoId) {
+		this.alunoId = alunoId;
+	}
+
+	public Integer getHoraId() {
+		return horaId;
+	}
+
+	public void setHoraId(Integer horaId) {
+		this.horaId = horaId;
+	}
+	
+	
+	
+}

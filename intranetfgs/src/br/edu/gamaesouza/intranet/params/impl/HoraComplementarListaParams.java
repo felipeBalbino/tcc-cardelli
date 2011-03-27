5927aa1ad3ef4861a44a8f9070aed01dc02b942f
svn: gabrielcardelli git: gabrielcardelli <gabrielcastilho.cardelli@gmail.com>
2011-03-27 00:19:26 +0000

    Adicionado método getHora()

--- intranetfgs/src/br/edu/gamaesouza/intranet/params/impl/HoraComplementarListaParams.java
+++ intranetfgs/src/br/edu/gamaesouza/intranet/params/impl/HoraComplementarListaParams.java
@@ -1,6 +1,8 @@
 package br.edu.gamaesouza.intranet.params.impl;
 
+import br.edu.gamaesouza.intranet.bean.Hora;
 import br.edu.gamaesouza.intranet.params.HoraComplementarParams;
+import br.edu.gamaesouza.intranet.utils.IntranetException;
 
 public class HoraComplementarListaParams implements HoraComplementarParams {
 
@@ -16,6 +18,12 @@ public class HoraComplementarListaParams implements HoraComplementarParams {
 	
 	public void setId(String id) {
 		this.id = Integer.parseInt(id);
+	}
+
+	@Override
+	public Hora getHora() throws IntranetException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- intranetfgs/src/br/edu/gamaesouza/intranet/params/impl/HoraComplementarNovoParams.java
+++ intranetfgs/src/br/edu/gamaesouza/intranet/params/impl/HoraComplementarNovoParams.java
@@ -1,7 +1,66 @@
 package br.edu.gamaesouza.intranet.params.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.gamaesouza.intranet.bean.Aluno;
+import br.edu.gamaesouza.intranet.bean.Hora;
+import br.edu.gamaesouza.intranet.bean.HoraComplementar;
+import br.edu.gamaesouza.intranet.dao.HoraDAO;
+import br.edu.gamaesouza.intranet.dao.PessoaDAO;
 import br.edu.gamaesouza.intranet.params.HoraComplementarParams;
+import br.edu.gamaesouza.intranet.utils.IntranetException;
 
 public class HoraComplementarNovoParams implements HoraComplementarParams {
+	
+	private Integer atividadeId;
+	private Integer numeroHoras;
+	private String nomeEvento;
+	private Aluno aluno;
+	
+	@Autowired private PessoaDAO pessoaDAO;
+	@Autowired private HoraDAO horaDAO;
 
+	public Integer getAtividadeId() {
+		return atividadeId;
+	}
+
+	public void setAtividadeId(Integer atividadeId) {
+		this.atividadeId = atividadeId;
+	}
+
+	public Integer getNumeroHoras() {
+		return numeroHoras;
+	}
+
+	public void setNumeroHoras(Integer numeroHoras) {
+		this.numeroHoras = numeroHoras;
+	}
+
+	public Hora getHora() throws IntranetException {
+		HoraComplementar horaComplementar = new HoraComplementar();
+		horaComplementar.setAluno(pessoaDAO.getAlunoById(aluno.getId()));
+		horaComplementar.setAtividade(horaDAO.getAtividadeById(atividadeId));
+		horaComplementar.setNomeEvento(nomeEvento);
+		horaComplementar.setNumeroHoras(numeroHoras);
+		return horaComplementar;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public String getNomeEvento() {
+		return nomeEvento;
+	}
+
+	public void setNomeEvento(String nomeEvento) {
+		this.nomeEvento = nomeEvento;
+	}
+
+	
+	
 }
